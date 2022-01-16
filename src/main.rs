bba2ac107a88bb9eb5f1ccf62d34632f8fcdb901
dspoerr <dawnspoerr@gmail.com>
2022-01-16 14:46:49 -0600

    user can input value as query. results are limited and exit feature not yet working

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,83 @@
 use std::io::Write;
 
 extern crate walkdir;
+extern crate tantivy;
 use walkdir::WalkDir;
 
-fn main() {
+use tantivy::collector::TopDocs;
+use tantivy::query::QueryParser;
+use tantivy::schema::*;
+use tantivy::Index;
+use tantivy::ReloadPolicy;
+use tantivy::doc;
+use std::path::Path;
+
+
+fn main() -> tantivy::Result<()> {
     let mut output = std::fs::File::create("list-of-files.txt").expect("Couldn't create file!");
 
-    for files in WalkDir::new("C:\\").into_iter().filter_map(|files| files.ok())
+    let index_path = tantivy::directory::MmapDirectory::open("C:\\Users\\dawns\\Documents\\rust\\search-by-tag\\index")?;
+
+    let mut schema_builder = Schema::builder();
+    
+    let title = schema_builder.add_text_field("title", TEXT | STORED);
+    let body = schema_builder.add_text_field("body", TEXT);
+    let schema = schema_builder.build();
+
+    let index = Index::open_or_create(index_path, schema.clone())?;
+
+    let mut index_writer = index.writer(50_000_000)?;
+
+    for files in WalkDir::new("C:\\Users\\dawns\\Documents\\rust\\search-by-tag").into_iter().filter_map(|files| files.ok())
     {
         if files.metadata().unwrap().is_file()
         {
+            index_writer.add_document(doc!(
+                title => files.file_name().to_str().unwrap(),
+                body => files.path().to_str().unwrap()
+            ));
+
+            /*
             output.write(files.path().display().to_string().as_bytes()).expect("Failed to write directory!");
             output.write("\n".as_bytes());
+            */
             println!("{}", files.path().display());
+
         }
     }
-
     println!("Directory probe completed.\n");
+
+    index_writer.commit()?;
+
+    let reader = index.reader()?;
+
+    let searcher = reader.searcher();
+
+    let query_parser = QueryParser::for_index(&index, vec![title, body]);
+
+    let mut newSearch = true;
+
+    let exitTerm = "exit";
+    while newSearch != false
+    {
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+
+        if line.as_str() == exitTerm
+        {
+            newSearch = false;
+            continue;
+        }
+
+        let mut query = query_parser.parse_query(&line.as_str())?;
+
+        let mut top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
+    
+        for (_score, doc_address) in top_docs {
+            // Retrieve the actual content of documents given its `doc_address`.
+            let retrieved_doc = searcher.doc(doc_address)?;
+            println!("{}", schema.to_json(&retrieved_doc));
+        }    
+    }
+    Ok(())
 }
